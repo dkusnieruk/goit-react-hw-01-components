4b047f05994c0c79e41a27464a137a131a128d42
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-12-11 08:09:40 +0100

    update task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
+import {Profile} from './components/Profile';
 import './index.css';
+import './profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+ 
+    <Profile/>
   </React.StrictMode>
 );
