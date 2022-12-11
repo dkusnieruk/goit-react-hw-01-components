d6bb67ae032593f4923ac795c089986846187fd9
dkusnieruk <dariusz.kusnieruk@wp.pl>
2022-12-11 12:08:45 +0100

    update tasks

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,39 +1,49 @@
 import user from '../json/user.json';
+import PropTypes from "prop-types";
 
-
-
-export const Profile = () => {
+export const Profile = props => {
 
     console.log(user.avatar);
+    
     return (
       <div className="profile">
   <div className="description">
     <img
-      src={user.avatar}
-      alt={user.username}
+      src={props.avatar}
+      alt={props.username}
       className="avatar"
     />
-    <p className="name">{user.username}</p>
-    <p className="tag">@{user.tag}</p>
-    <p className="location">{user.location}</p>
+    <p className="name">{props.username}</p>
+    <p className="tag">@{props.tag}</p>
+    <p className="location">{props.location}</p>
   </div>
 
   <ul className="stats">
     <li>
       <span className="label">Followers</span>
-      <span className="quantity">{user.stats.followers}</span>
+      <span className="quantity">{props.stats.followers}</span>
     </li>
     <li>
       <span className="label">Views</span>
-      <span className="quantity">{user.stats.views}</span>
+      <span className="quantity">{props.stats.views}</span>
     </li>
     <li>
       <span className="label">Likes</span>
-      <span className="quantity">{user.stats.likes}</span>
+      <span className="quantity">{props.stats.likes}</span>
     </li>
   </ul>
 </div>    );
 
   };
   
-  
+Profile.propTypes ={
+    avatar : PropTypes.string,
+    username: PropTypes.string,
+    tag :PropTypes.string,
+    location :PropTypes.string,
+    followers :PropTypes.number,
+    views :PropTypes.number,
+    likes:PropTypes.number,
+    
+
+};
--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { App } from 'components/App';
 import {Profile} from './components/Profile';
+import { Stats } from 'components/Stats';
 import './index.css';
 import './profile.css';
+import './stats.css'
+import data from './json/data.json';
+import user from './json/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
  
-    <Profile/>
+<Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+    <Stats stats={data} />
   </React.StrictMode>
 );
